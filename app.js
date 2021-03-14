8cfb50863de5f86b492de3d4849f93a0d4b98d68
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2021-03-14 12:27:36 -0600

    Pre hash/salt

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+//bcrypt
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Module Settings =========================================================
+mongoose.connect('mongodb://127.0.0.1/secrets', {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
+    if(err){
+        console.log(error);
+    };
+});
+
+// Mongoose Schemas ========================================================
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+// Mongoose Models =========================================================
+const User = mongoose.model('User', userSchema);
+
+// Mongoose Functions ======================================================
+const createNewUser = function(username, password){
+    return new Promise((resolve, reject)=>{
+        console.log("name: " + username)
+        const newUser = new User({
+            email: username,
+            password: password
+        });
+        
+        newUser.save((err, result)=>{
+            if(!err){
+                resolve(true);
+            }else{
+                console.warn(err);
+                reject();
+            };
+        });
+    })
+};
+
+const userLogin = function(username, password){
+    return new Promise((resolve, reject)=>{
+        User.findOne({email: username, password:password}, (err, results)=>{
+            if(!err){
+                resolve(results);
+            }else{
+                console.warn(err);
+            };
+        });
+    });
+};
+
+// Get Routes ==============================================================
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+
+app.get('/login', (req, res)=>{
+    res.render('login');
+});
+
+app.get('/register', (req, res)=>{
+    res.render('register');
+});
+
+// app.get('/secrets', (req, res)=>{
+//     res.render('secrets');
+// });
+
+// app.get('/submit', (req, res)=>{
+//     res.render('submit');
+// });
+
+// Post Routes ==============================================================
+app.post('/register', async (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{
+        result = await createNewUser(username, password);
+        if(result){
+            res.render('secrets');
+        };
+    }catch(err) {
+        console.warn(err);
+    };
+});
+
+app.post('/login', async (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    try{
+        const result = await userLogin(username, password);
+        if(result){
+            res.render('secrets');
+        }else{
+            console.log('fail');
+        };
+    }catch(err){
+        console.warn(err);
+    };
+
+});
+
+// Server ===================================================================
+app.listen(3000, ()=>{
+    console.log("localhost:3000");
+});
