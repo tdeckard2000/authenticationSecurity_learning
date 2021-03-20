4f8f10559a0e841c85f7991a71aee1bacd2e1afa
TEDSPOWERHOUSE\Trent <tdecakrd2000@yahoo.com>
2021-03-19 20:14:52 -0600

    setup mongoose-encryption

--- app.js
+++ app.js
@@ -7,33 +7,34 @@ require('dotenv').config();
 //bcrypt
 
 const app = express();
-console.log(process.env.SESSION_SECRET)
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Module Settings =========================================================
+// Module Settings ================================================================================================
 mongoose.connect('mongodb://127.0.0.1/secrets', {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
     if(err){
-        console.log(error);
+        console.warn(error);
     };
 });
 
-// userSchema.plugin(encrypt, {encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['password']});
-
-// Mongoose Schemas ========================================================
+// Mongoose Schemas ===============================================================================================
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-// Mongoose Models =========================================================
+// Mongoose-Encryption Settings ===================================================================================
+const encKey = process.env.BASE64_32;
+const sigKey = process.env.BASE64_64;
+userSchema.plugin(encrypt, {encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['password']});
+
+// Mongoose Models ================================================================================================
 const User = mongoose.model('User', userSchema);
 
-// Mongoose Functions ======================================================
+// Mongoose Functions =============================================================================================
 const createNewUser = function(username, password){
     return new Promise((resolve, reject)=>{
-        console.log("name: " + username)
         const newUser = new User({
             email: username,
             password: password
@@ -50,9 +51,9 @@ const createNewUser = function(username, password){
     })
 };
 
-const userLogin = function(username, password){
+const userLogin = function(username){
     return new Promise((resolve, reject)=>{
-        User.findOne({email: username, password:password}, (err, results)=>{
+        User.findOne({email: username}, (err, results)=>{
             if(!err){
                 resolve(results);
             }else{
@@ -62,7 +63,7 @@ const userLogin = function(username, password){
     });
 };
 
-// Get Routes ==============================================================
+// Get Routes =====================================================================================================
 app.get('/', (req, res)=>{
     res.render('home');
 });
@@ -75,6 +76,10 @@ app.get('/register', (req, res)=>{
     res.render('register');
 });
 
+app.get('/logout', (req, res)=>{
+    res.send('<h3 style="padding: 30px 0; text-align: center;">You have been successfully logged out.</h3>');
+});
+
 // app.get('/secrets', (req, res)=>{
 //     res.render('secrets');
 // });
@@ -83,7 +88,7 @@ app.get('/register', (req, res)=>{
 //     res.render('submit');
 // });
 
-// Post Routes ==============================================================
+// Post Routes =====================================================================================================
 app.post('/register', async (req, res)=>{
     const username = req.body.username;
     const password = req.body.password;
@@ -104,10 +109,10 @@ app.post('/login', async (req, res)=>{
     
     try{
         const result = await userLogin(username, password);
-        if(result){
+        if(result.password === password){
             res.render('secrets');
         }else{
-            console.log('fail');
+            console.warn('fail');
         };
     }catch(err){
         console.warn(err);
@@ -115,8 +120,8 @@ app.post('/login', async (req, res)=>{
 
 });
 
-// Server ===================================================================
+// Server ==========================================================================================================
 app.listen(3000, ()=>{
-    console.log("localhost:3000");
+    console.warn("localhost:3000");
 });
 
